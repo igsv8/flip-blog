186c84010364f525a2672e20b24b24ef09b6bb61
Iosif <15144776+igsv8@users.noreply.github.com>
2024-09-26 01:18:00 +0300

    feat: add icons to blog post card

--- src/app/components/blog-post/blog-post.component.ts
+++ src/app/components/blog-post/blog-post.component.ts
@@ -1,11 +1,14 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { heroHandThumbUp, heroHandThumbDown, heroEye } from '@ng-icons/heroicons/outline';
 import { Blogpost } from '../../models/blogpost';
 
 @Component({
   selector: 'app-blog-post',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgIconComponent],
+  providers: [provideIcons({ heroHandThumbUp, heroHandThumbDown, heroEye })],
   templateUrl: './blog-post.component.html',
   styleUrls: ['./blog-post.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
