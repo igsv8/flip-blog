9219aaa6ea2ca386eb5966d57566895ca683e36d
Iosif <15144776+igsv8@users.noreply.github.com>
2024-09-26 14:13:14 +0300

    feat: add comments to blog posts

--- src/app/components/post-detail/post-detail.component.ts
+++ src/app/components/post-detail/post-detail.component.ts
@@ -2,33 +2,39 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { BlogPostComponent } from '../blog-post/blog-post.component';
 import { AsyncPipe, CommonModule } from '@angular/common';
 import { Blogpost } from '../../models/blogpost';
-import { filter, Observable, take } from 'rxjs';
+import { filter, Observable, switchMap, take } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { selectSelectedPost } from '../../store/posts.selectors';
 import * as fromPostsActions from '../../store/posts.actions';
 import * as fromPostsSelectors from '../../store/posts.selectors';
 import { ActivatedRoute } from '@angular/router';
+import { CommentComponent } from "../comment/comment.component";
+import { CommentModel } from '../../models/comment.model';
+import { PostsService } from '../../services/posts.service';
 
 @Component({
   selector: 'app-post-detail',
   templateUrl: './post-detail.component.html',
   styleUrls: ['./post-detail.component.scss'],
   standalone: true,
-  imports: [CommonModule, BlogPostComponent, AsyncPipe],
+  imports: [CommonModule, BlogPostComponent, AsyncPipe, CommentComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PostDetailComponent implements OnInit {
   blogpost$: Observable<Blogpost>;
   loading$: Observable<Boolean>;
   error$: Observable<string | null>;
+  comments$: Observable<CommentModel[]>;
 
   constructor(
     private store: Store,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private postsService: PostsService
   ) {
     this.blogpost$ = this.store.select(selectSelectedPost).pipe(filter(Boolean));
     this.loading$ = this.store.select(fromPostsSelectors.selectLoading);
     this.error$ = this.store.select(fromPostsSelectors.selectError);
+    this.comments$ = this.blogpost$.pipe(switchMap(blogpost => this.postsService.getCommentsByID(blogpost.id)));
   }
 
   ngOnInit(): void {
@@ -46,6 +52,8 @@ export class PostDetailComponent implements OnInit {
         });
       }
     });
+
+
 
   }
 
--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -3,12 +3,14 @@ import { HttpClient } from '@angular/common/http';
 import { Blogpost } from '../models/blogpost';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { CommentModel } from '../models/comment.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
   private postsUrl = 'https://dummyjson.com/posts';
+  private commentsUrl = 'https://dummyjson.com/comments/post';
 
   constructor(private http: HttpClient) { }
 
@@ -21,6 +23,10 @@ export class PostsService {
 
   getBlogPostById(id: number): Observable<Blogpost> {
     return this.http.get<Blogpost>(`${this.postsUrl}/${id}`);
+  }
+
+  getCommentsByID(id: number): Observable<CommentModel[]> {
+    return this.http.get<CommentModel[]>(`${this.commentsUrl}/${id}`).pipe(map((response: any) => response.comments));
   }
 
   createPost(post: Partial<Blogpost>): Observable<Blogpost> {
